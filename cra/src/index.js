a304391ebd0954b64905e6ed31b03f57032a44f3
pacokwon <haechank@gmail.com>
2020-10-24 16:29:51 +0900

    update: remove boilerplate and add routing

--- cra/src/index.js
+++ cra/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import DogsList from './components/DogsList'
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <App />
+      </Route>
+      <Route exact path="/dogs">
+        <DogsList />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
