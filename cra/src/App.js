0af5c7db7522911cb41f0e98fe3373d829282541
pacokwon <haechank@gmail.com>
2020-10-25 02:05:26 +0900

    add: navigation in App component

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import './App.css';
+import { Link } from 'react-router-dom'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-        Hello
+    <div className="app">
+      <ul>
+        <li>
+          <Link to="/counter">
+            Counter 컴포넌트
+          </Link>
+        </li>
+        <li>
+          <Link to="/dogs">
+            API 요청을 통해 데이터 불러오기
+          </Link>
+        </li>
+        <li>
+          <Link to="/dogslist">
+            Dogs List
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
